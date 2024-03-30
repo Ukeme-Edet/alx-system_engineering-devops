795da00643669cd9220d539afda474ad51619846
Ukeme Edet <ukemeedet2207@gmail.com>
2024-03-30 17:49:27 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -3,6 +3,10 @@
 # Requirements:
 #   Install `flask`
 #   Version must be 2.1.0
+package { 'werkzeug':
+  ensure   => '2.1.1',
+  provider => 'pip3'
+}
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3'
