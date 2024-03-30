ae1964b4926e260adef05b079117c9a045ebad0a
Ukeme Edet <ukemeedet2207@gmail.com>
2024-03-30 17:40:34 +0100

    Install a package

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -5,6 +5,6 @@
 #   Version must be 2.1.0
 exec { 'install_flask_2.1.0':
   command => 'pip3 install flask==2.1.0',
-  path    => ['/usr/local/bin', '/usr/bin', '/bin', '/usr/local/sbin', '/usr/sbin', '/sbin'],
+  path    => '/usr/local/bin',
   unless  => 'pip3 show flask | grep Version | grep 2.1.0'
 }
