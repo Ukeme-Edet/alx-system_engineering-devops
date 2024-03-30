f6070a9f26064e6043741fa494456ec08336165b
Ukeme Edet <ukemeedet2207@gmail.com>
2024-03-30 19:04:59 +0100

    Client configuration file (w/ Puppet)

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,20 +1,11 @@
 # A puppet script to configure the Local SSH client so that we can connect to the remote SSH server without password.
 
-exec { 'ssh-keygen':
-  command => 'ssh-keygen -b 4096 -N "" -f ~/.ssh/id_rsa',
-  creates => '/root/.ssh/id_rsa',
+file_line { 'IdentityFile':
+  ensure => present,
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/id_rsa',
 }
-
-exec { 'copy_pub_to_auth':
-  command => 'cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys',
-  require => Exec['ssh-keygen'],
-}
-
-exec { 'add_lines'
-  commands => [
-    'echo "Host *" >> ~/.ssh/config',
-    'echo "StrictHostKeyChecking no" >> ~/.ssh/config',
-    'echo "UserKnownHostsFile=/dev/null" >> ~/.ssh/config',
-  ],
-  require => Exec['copy_pub_to_auth'],
+file_line { 'disable password authentication':
+  path => '/etc/ssh/ssh_config',
+  line => 'PasswordAuthentication no',
 }
