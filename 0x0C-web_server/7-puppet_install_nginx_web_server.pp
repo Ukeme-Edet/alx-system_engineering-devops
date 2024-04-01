54cf350025e2837a7ecd4ec2065dba6d426b3268
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-01 18:57:10 +0100

    Install Nginx web server (w/ Puppet)

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -7,7 +7,7 @@ file_line { 'redirect_me':
   ensure => 'present',
   path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  line   => 'rewrite ^/redirect_me https://youtu.be/dQw4w9WgXcQ?si=c5SSrhdJz17xP0kV permanent;',
 }
 
 file { '/var/www/html/index.html':
