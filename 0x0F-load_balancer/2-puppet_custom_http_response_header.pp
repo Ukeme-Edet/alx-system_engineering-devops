8edff650fd8c5a17da6b9b0dba11b72314c3fcfe
Ukeme Edet <ukemeedet2207@gmail.com>
2024-04-03 03:52:40 +0100

    Add a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,32 +1,35 @@
-# Install Nginx web server (w/ Puppet) and add Custom HTTP Header
-exec { 'apt-update':
+# puppet manifest creating a custom HTTP header response
+exec { 'apt-get-update':
   command => '/usr/bin/apt-get update',
 }
 
 package { 'nginx':
-  ensure => installed,
+  ensure  => installed,
+  require => Exec['apt-get-update'],
 }
 
-file_line { 'redirect_me':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://youtu.be/dQw4w9WgXcQ?si=c5SSrhdJz17xP0kV permanent;',
+file_line { 'a':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+  require => Package['nginx'],
 }
 
-file_line { 'add_custom_header':
-  ensure => 'present',
-  path   => '/etc/nginx/nginx.conf',
-  after  => 'listen 80 default_server;',
-  line   => 'add_header X-Served-By $hostname;',
+file_line { 'b':
+  ensure  => 'present',
+  path    => '/etc/nginx/sites-available/default',
+  after   => 'listen 80 default_server;',
+  line    => 'add_header X-Served-By $hostname;',
+  require => Package['nginx'],
 }
 
 file { '/var/www/html/index.html':
   content => 'Hello World!',
+  require => Package['nginx'],
 }
 
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
 }
-
