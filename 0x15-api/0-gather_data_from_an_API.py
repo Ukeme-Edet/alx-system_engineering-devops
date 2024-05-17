01fb7158f32559f367f403836dc406dc0973f83c
Ukeme Edet <ukemeedet2207@gmail.com>
2024-05-17 13:37:43 +0100

    Gather data from an API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+This script gathers data from an API and prints the tasks completed by an\
+    employee.
+
+Usage: python3 0-gather_data_from_an_API.py [employee_id]
+"""
+
+from sys import argv
+import requests
+
+if __name__ == "__main__":
+    with requests.Session() as s:
+        url = "https://jsonplaceholder.typicode.com/users/{}".format(argv[2])
+        todos = s.get(f"{url}/todos").json()
+        user = s.get(url).json()
+        done_todos = [todo for todo in todos if todo["completed"]]
+        print(
+            "Employee {} is done with tasks({}/{}):".format(
+                user["name"], len(done_todos), len(todos)
+            )
+        )
+        print(
+            "\n".join([f"\t {done_todo['title']}" for done_todo in done_todos])
+        )
