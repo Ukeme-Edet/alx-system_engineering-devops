19539c520b148860a6b1cab097b0552e0d34bb74
Ukeme Edet <ukemeedet2207@gmail.com>
2024-05-26 20:18:18 +0100

    Refactor code to handle 404 response when retrieving subreddit subscribers count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,6 +20,6 @@ def number_of_subscribers(subreddit):
         session.headers = {"User-Agent": "Mozilla/5.0"}
         url = f"https://www.reddit.com/r/{subreddit}/about.json"
         response = session.get(url)
-        if response.status_code == 200 and response.status_code != 302:
-            return response.json().get("data").get("subscribers")
-        return 0
+        if response.status_code == 404:
+            return 0
+        return response.json().get("data").get("subscribers")
