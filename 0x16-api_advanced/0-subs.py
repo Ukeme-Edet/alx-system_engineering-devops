8396f3cceaa1763c3d4921a7dbb54301670f3f88
Ukeme Edet <ukemeedet2207@gmail.com>
2024-05-26 20:02:18 +0100

    chore: Add function to retrieve number of subscribers for a subreddit

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!usr/bin/python3
+"""
+This module contains a function to retrieve the number of subscribers for a given subreddit.
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    Returns the number of subscribers for a given subreddit.
+
+    Args:
+            subreddit (str): The name of the subreddit.
+
+    Returns:
+            int: The number of subscribers for the subreddit.
+    """
+    with requests.Session() as session:
+        session.headers = {"User-Agent": "Mozilla/5.0"}
+        url = f"https://www.reddit.com/r/{subreddit}/about.json"
+        response = session.get(url)
+        data = response.json().get("data")
+        if data:
+            return data.get("subscribers")
+        return 0
