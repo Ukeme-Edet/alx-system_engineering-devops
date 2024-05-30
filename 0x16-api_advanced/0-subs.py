15e8f73ddcc8b91582f6dcef7534566ebec2e2e5
Ukeme Edet <ukemeedet2207@gmail.com>
2024-05-30 13:47:53 +0100

    Refactor code to improve retrieval of subreddit subscribers count

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -19,10 +19,7 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {"User-Agent": "Mozilla/5.0"}
     with requests.Session() as sess:
-        try:
-            response = sess.get(url, headers=headers, allow_redirects=False)
-            if response.status_code == 200:
-                return response.json().get("data").get("subscribers")
-            return 0
-        except Exception:
-            return 0
+        response = sess.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            return response.json().get("data").get("subscribers")
+        return 0
