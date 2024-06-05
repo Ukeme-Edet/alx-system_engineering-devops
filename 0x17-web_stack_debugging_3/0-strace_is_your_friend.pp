c495d4628b77b4ad92ed77cf3f30cf3c273457ec
Ukeme Edet <ukemeedet2207@gmail.com>
2024-06-05 23:31:01 +0100

    Refactor code to fix bad `phpp` extensions in WordPress file `wp-settings.php`

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,33 +1,6 @@
-# A script to find out why Apache is returning a 500 error using strace
-# Usage: ./0-strace_is_your_friend.pp
-# Example: curl -sI
-#
-# This script will use strace to find out why Apache is returning a 500 error
-# when you try to access the root of the server. It will then fix the issue
-# and make sure Apache is running.
-exec { 'apt-get-update':
-  command => '/usr/bin/apt-get update',
-}
+# Fixes bad `phpp` extensions to `php` in the WordPress file `wp-settings.php`.
 
-package { 'apache2':
-  ensure  => installed,
-  require => Exec['apt-get-update'],
-}
-
-file_line { 'a':
-  ensure  => 'present',
-  path    => '/etc/apache2/sites-available/000-default.conf',
-  after   => '<VirtualHost *:80>',
-  line    => 'ServerName localhost',
-  require => Package['apache2'],
-}
-
-file { '/var/www/html/index.html':
-  content => 'Hello World!',
-  require => Package['apache2'],
-}
-
-service { 'apache2':
-  ensure  => running,
-  require => Package['apache2'],
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/',
 }
